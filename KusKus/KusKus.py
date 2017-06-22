f3ce87b95cc3db8fcfa6b8d07deff1adca8034b8
johnsmith17 <johnsmithsparemail@gmail.com>
2017-06-22 15:13:57 +0200

    webpage

--- KusKus/KusKus.py
+++ KusKus/KusKus.py
@@ -1,1 +1,30 @@
-__author__ = 'Marko'
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+DATABASE = 'D:\projects\python projects\webdev\KusKus\kuskus.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+init_db()
+
+if __name__ == '__main__':
+    app.run()
